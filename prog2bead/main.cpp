4bc20b98083e0abdf732e227b2db6cd0f21b9eaa
Attila Herbert <herbertatus@gmail.com>
2017-01-02 12:32:56 +0100

    Initial Commit

---
+++ prog2bead/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prog2bead
+//
+//  Created by Atus on 1/2/17.
+//  Copyright © 2017 ha. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
