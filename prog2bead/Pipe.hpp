1cf7b92215dd7f7452622a2fa194adbb8a769d93
Attila Herbert <herbertatus@gmail.com>
2017-01-02 13:27:24 +0100

    f

---
+++ prog2bead/Pipe.hpp
@@ -0,0 +1,14 @@
+//
+//  Pipe.hpp
+//  prog2bead
+//
+//  Created by Atus on 1/2/17.
+//  Copyright © 2017 ha. All rights reserved.
+//
+
+#ifndef Pipe_hpp
+#define Pipe_hpp
+
+#include <stdio.h>
+
+#endif /* Pipe_hpp */
